b9eca06f77b41c93e5806e88b7ccc1c2cefcbf14
Roblezno <pablorimure@gmail.com>
2026-05-14 00:09:04 +0200

    feat: implemented lualine and todo-comments pluginsç

---
+++ lua/plugins/todo-comments.lua
@@ -0,0 +1,27 @@
+-- ============================================================================
+-- TITLE: todo-comments
+-- ABOUT: Better comments for adding notes, warnings, bugs ...
+-- LINKS:
+--  > github: https://github.com/folke/todo-comments.nvim
+-- ============================================================================
+
+
+return {
+    {
+        "folke/todo-comments.nvim",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        opts = {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+        }
+    }
+}
+
+-- PERF:
+-- HACK:
+-- TODO: 
+-- NOTE:
+-- FIX:
+-- WARNING:
+-- TEST:
