fd4f236fc736f56e112e5168c56f749c76e16fca
Roblezno <pablorimure@gmail.com>
2026-05-13 12:15:49 +0200

    feat: global key implemented as "<space>". Updated README

---
+++ lua/config/globals.lua
@@ -0,0 +1,8 @@
+-- ================================================================================================
+-- TITLE : globals
+-- ABOUT : different global & local leaders
+-- ================================================================================================
+
+-- NeoVim Leader key, I pick <space> since that's what I'm used to
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
