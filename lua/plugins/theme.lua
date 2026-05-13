2a3a2972649cebae0d1d2c78ce0a317c5b8e5c09
Roblezno <pablorimure@gmail.com>
2026-05-13 23:38:12 +0200

    hotfix: fixed theme not loading as intended

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,5 +1,20 @@
 return {
     "rebelot/kanagawa.nvim",
-    lazy = false, -- Disable lazy load, theme must be high prior
-    priority = 1000, -- Ensure high prior
+    lazy = false,
+    priority = 1000,
+    -- Optional config
+    opts = {
+        transparent = true, 
+        commentStyle = { italic = true },
+        theme = "wave",
+    },
+    -- Theme must be initialized with config func
+    -- Passing opt params
+    config = function(_, opts)
+        -- Setting up theme with custom opts
+        require("kanagawa").setup(opts) 
+        
+        -- Loading theme
+        vim.cmd("colorscheme kanagawa") 
+    end,
 }
