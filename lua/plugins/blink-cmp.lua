5d8f7eb2b36d122c3f3689c4cd24afdeb416c03a
Roblezno <pablorimure@gmail.com>
2026-05-19 15:25:24 +0200

    feat: noice plugin for better interface visuals implemented

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -37,7 +37,10 @@ return {
         preset = 'default',
         ['<Tab>'] = { 'select_next', 'fallback' },
         ['<S-Tab>'] = { 'select_prev', 'fallback' },
+        ['<S-CR>'] = { 'accept', 'fallback' },
     },
+
+    signature = { enabled = false },
 
     appearance = {
       -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
@@ -46,7 +49,9 @@ return {
     },
 
     -- (Default) Only show the documentation popup when manually triggered
-    completion = { documentation = { auto_show = false } },
+    completion = { 
+        documentation = { auto_show = false },
+    },
 
     -- Default list of enabled providers defined so that you can extend it
     -- elsewhere in your config, without redefining it, due to `opts_extend`
