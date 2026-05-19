6a648cfbd8a6bbde81330d6a6edd8609a7594fcb
Roblezno <pablorimure@gmail.com>
2026-05-20 00:44:57 +0200

    hotfix: fixed neotree from breakin on auto session load
    Neo tree was getting "trapped" as a hidden buffer when creating swapping
    git branches and could not be oppened from the same directory. I
    implemented keymaps for auto-session plugin to delete sessions in case
    this happends, it should remove the "trapped" buffer. Now restoring
    sessions must be done manually

--- lua/plugins/auto-session.lua
+++ lua/plugins/auto-session.lua
@@ -1,11 +1,18 @@
 return {
   "rmagatti/auto-session",
   lazy = false,
+  keys = {
+      -- Explicit commands for the current working directory
+    { "<leader>wr", "<cmd>AutoSession restore<CR>", desc = "Restore cwd session" },
+    { "<leader>ws", "<cmd>AutoSession save<CR>", desc = "Save cwd session" },
+    { "<leader>wd", "<cmd>AutoSession delete<CR>", desc = "Delete cwd session" },
+  },
 
   ---enables autocomplete for opts
   ---@module "auto-session"
   ---@type AutoSession.Config
   opts = {
+      auto_restore = false, -- disables restoring auto session on start
     -- suppressed_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
     -- log_level = 'debug',
   },
