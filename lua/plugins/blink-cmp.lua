a50c02e3813b8b2dd5f327dace6bd7e2e1baee1a
Roblezno <pablorimure@gmail.com>
2026-05-27 22:55:47 +0200

    hotfix: Force blink-cmp to use version 1.* (stable version)

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -8,6 +8,7 @@ return {
   'saghen/blink.cmp',
   -- optional: provides snippets for the snippet source
   dependencies = { 'rafamadriz/friendly-snippets' },
+  version = "1.*",
   opts = {
     -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
     -- 'super-tab' for mappings similar to vscode (tab to accept)
